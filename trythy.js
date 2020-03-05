e7c71313fda6c1c3d5a40988e99370dea379e1dc
sharjis <sharjis.khan07@gmail.com>
2020-03-05 20:40:48 +0600

    == vs ===

---
+++ trythy.js
@@ -0,0 +1,13 @@
+const first=2;
+const second='2';
+if(first===second){
+    console.log("true");
+}
+
+
+else{
+    console.log("false");
+}
+//0 ar false eki... == dile
+//=== dile check korbe tara ashole ki...
+
