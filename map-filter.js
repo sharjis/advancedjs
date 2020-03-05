bcec98bac1e46bf511c57910bfab165a9df8c2e5
sharjis <sharjis.khan07@gmail.com>
2020-03-05 21:50:09 +0600

    map-filter-find

---
+++ map-filter.js
@@ -0,0 +1,22 @@
+const numbers=[1,2,3,4,5,6,7,8];
+const output=[];
+//for(let i=0;i<numbers.length;i++){
+  //  const element=numbers[i];
+    //const result=element*element;
+   // output.push(result);
+//}
+//console.log(output);
+// function square(num){
+//     result=num*num;
+//     return result;
+//}
+
+//const result=numbers.map(function(element){
+    //return element * element;
+//})
+//console.log(result);
+//const result=numbers.map(x =>x*x );
+//console.log(result);
+
+const result=numbers.filter(x=> x<5);
+console.log(result);
