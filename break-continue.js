35c5ffb0d931b5deb9d84d9f92f337c7a8ab560f
sharjis <sharjis.khan07@gmail.com>
2020-03-06 00:26:44 +0600

    break-continue

---
+++ break-continue.js
@@ -0,0 +1,8 @@
+const numbers=[1,2,3,4,5,6,7,8,9];
+for(let i=0;i<numbers.length;i++){
+    console.log(numbers[i]);
+    if(numbers[i]>6){
+        break;
+    }
+}
+//break mane thamaya deya ar continue mane oigula bade shob
